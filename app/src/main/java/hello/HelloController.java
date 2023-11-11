f6e95553c8ee185ff35573dab13411b1c40803ea
Siddwin456 <124484731+Siddwin456@users.noreply.github.com>
2023-11-11 18:32:47 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Hello to Siddwin from Spring Boot!";
     }   
 }
